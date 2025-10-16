204d279065ab64d29efff68d987328b6ddbd52b8
denis2025-beep <musiiged@nm-aist.ac.tz>
2025-10-16 12:30:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React, { useEffect, useState } from "react";
 import { ref, onValue } from "firebase/database";
-import database from "../firebase";
+import database from "./firebase";
 
 function App() {
   const [sensorData, setSensorData] = useState({});
